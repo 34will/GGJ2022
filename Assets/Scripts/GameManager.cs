395d8b834441e3f440723e111cf168ef7443e9cd
34will <wgilmour@hotmail.co.uk>
2022-01-22 23:41:34 +0000

    Things that are new: yes, Gates! UI!

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace GGJ2020
+{
+    public class GameManager : MonoBehaviour
+    {
+        public void Start()
+        {
+            
+        }
+
+        public void Update()
+        {
+            
+        }
+    }
+}
---
+++ Assets/Scripts/Rotater.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+namespace GGJ2020
+{
+    public class Rotater : MonoBehaviour
+    {
+        private float rotation = 0.0f;
+
+        public float RotateSpeed = 0.5f;
+
+        // Update is called once per frame
+        public void Update()
+        {
+            rotation += RotateSpeed * Time.deltaTime;
+            transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotation);
+        }
+    }
+}
